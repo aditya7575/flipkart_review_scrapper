52885304d383491e5da422025354cff2b5e6cec4
Aditya singh tomar <tomar.tomar7755@gmail.com>
2024-01-23 01:19:46 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import scrapperModule as sm
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+            p=sm.Product_Scrapper(request.form['content'])
+            reviews=p.savingComments()
+            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    # return render_template('results.html')
+
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8001, debug=True)
+	#app.run(debug=True)
